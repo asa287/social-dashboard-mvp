316338daf6c721edaa3c4dcbf3cd9b5cca14272b
Samantha Sun <samanthasunxf@outlook.com>
2026-02-14 10:54:51 -0800

    Fix: Add custom dynamic not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+
+import Link from "next/link"
+
+export const dynamic = 'force-dynamic'
+
+export default function NotFound() {
+    return (
+        <div className="flex h-screen flex-col items-center justify-center gap-4">
+            <h2 className="text-2xl font-bold">Not Found</h2>
+            <p>Could not find requested resource</p>
+            <Link href="/" className="text-blue-500 hover:underline">
+                Return Home
+            </Link>
+        </div>
+    )
+}
